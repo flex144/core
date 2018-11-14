76b3f3195332af75e4441eee7564c8a9587a9cd5
Felix Wilhelm <wilhel40@gw.uni-passau.de>
2018-11-14 12:44:05 +0100

    T2-8 #in-progress created index.html + controller

---
+++ src/main/java/de/ep/team2/core/controller/IndexController.java
@@ -0,0 +1,13 @@
+package de.ep.team2.core.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+@RequestMapping("/")
+    String index(){
+    return "index";
+}
+}
