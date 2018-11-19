fd1dd834a7a6b4c24e454bafcbb5b2e77c6f1d77
reissi01 <REISSI01@gw.uni-passau.de>
2018-11-19 16:15:52 +0100

    #T2-18 #in-progress Renaming and restructuring Templates

--- src/main/java/de/ep/team2/core/controller/IndexController.java
+++ src/main/java/de/ep/team2/core/controller/IndexController.java
@@ -2,7 +2,6 @@ package de.ep.team2.core.controller;
 
 import de.ep.team2.core.DbTest.User;
 import de.ep.team2.core.service.UserService;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -44,11 +43,6 @@ public class IndexController {
         return errorMessage;
     }
 
-    @RequestMapping(value = {"/search"}, method = RequestMethod.GET)
-    public String searchUser() {
-        return "Mod-UserSearch";
-    }
-
     @RequestMapping(value = {"/create"}, method=RequestMethod.GET)
     public String create(){
         return "formular_create_exercise";
@@ -56,20 +50,20 @@ public class IndexController {
 
     @RequestMapping(value = {"/usersearch"}, method = RequestMethod.GET)
     public String userSearch() {
-        return "mod-userSearch";
+        return "mod_user_search";
     }
 
     /**
      * This method searches for all Users in the Database and adds them to the model
      * of the Thymeleaf Html-Website.
      * @param model Adds the existing Users to the model, for Thymeleaf to work with.
-     * @return Returns mod-userSearch.html
+     * @return Returns mod_user_search.html
      */
     @RequestMapping(value = {"/usersearch"}, method = RequestMethod.POST)
     public String getUserList(Model model, @ModelAttribute("searchtext") String searchtext) {
         UserService userService = new UserService();
         model.addAttribute("users", userService.getAllUsers());
-        return "mod-userSearch";
+        return "mod_user_search";
     }
 
 
