56ce73d8c1b30165113c8af5535cceb38a98689b
Timo Heinrich <heinri30@gw.uni-passau.de>
2018-11-20 16:48:32 +0100

    T2-10 #in-progress added delete function an tests

--- src/test/java/de/ep/team2/core/CoreApplicationTests.java
+++ src/test/java/de/ep/team2/core/CoreApplicationTests.java
@@ -5,18 +5,20 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class CoreApplicationTests {
 
     @Autowired
     private TestRestTemplate restTemplate;
 
-	@Test
+    @Test
     public void getUser() {
         assertTrue(restTemplate.getForObject("/users/1", String.class)
                 .contains("Heinrich"));
@@ -27,13 +29,14 @@ public class CoreApplicationTests {
                 .contains("Reißig"));
     }
 
-    /*
     @Test
     public void deleteUser() {
         restTemplate.delete("http://localhost:8080/users/1");
-        assertTrue(restTemplate.getForObject("/users/1",String.class)
+        assertTrue(restTemplate.getForObject("/users/1", String.class)
                 .contains("Benutzer nicht gefunden!"));
+        HttpEntity<String> response = restTemplate.exchange(
+                "http://localhost:8080/users/1", HttpMethod.DELETE, null,
+                String.class);
+        assertTrue(response.getBody().contains("Benutzer existiert nicht!"));
     }
-    */
-
 }
