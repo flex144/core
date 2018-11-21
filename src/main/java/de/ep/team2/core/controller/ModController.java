e74e039e5ea6fcbcdfc690a4fc78c160fc40e376
Felix Wilhelm <wilhel40@gw.uni-passau.de>
2018-11-21 15:03:22 +0100

    T2-20 #in-progress Added Get-Controller for Mod-Websites

---
+++ src/main/java/de/ep/team2/core/controller/ModController.java
@@ -0,0 +1,41 @@
+package de.ep.team2.core.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/mod")
+public class ModController {
+
+    @RequestMapping(value= {"/home"}, method = RequestMethod.GET)
+    public String home() {
+        return "mod_startup_page_web";
+    }
+
+    @RequestMapping(value= {"/createplan"}, method = RequestMethod.GET)
+    public String createPlan() {
+        return "mod_create_plan";
+    }
+
+    @RequestMapping(value = {"/createexercise"}, method = RequestMethod.GET)
+    public String createExercise() {
+        return "mod_create_exercise";
+    }
+
+    @RequestMapping(value = {"/searchexercise"}, method = RequestMethod.GET)
+    public String searchExercise() {
+        return "mod_exercise_search";
+    }
+
+    @RequestMapping(value = {"/searchplan"}, method = RequestMethod.GET)
+    public String searchPlan() {
+        return "mod_plan_search";
+    }
+
+    @RequestMapping(value = {"/searchuser"}, method = RequestMethod.GET)
+    public String searchUser() {
+        return "mod_user_search";
+    }
+
+}
