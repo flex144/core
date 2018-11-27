6e34ffe236444f5e3174523d18cffaddb5263155
reissi01 <REISSI01@gw.uni-passau.de>
2018-11-27 16:57:27 +0100

    #T2-56 #in-progress Renamed and deleted navbar fragments, Added th:replace for navbar in all pages, renamed mod startup page and deleted the old one

--- src/main/java/de/ep/team2/core/controller/ModController.java
+++ src/main/java/de/ep/team2/core/controller/ModController.java
@@ -3,7 +3,6 @@ package de.ep.team2.core.controller;
 import de.ep.team2.core.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -16,7 +15,7 @@ public class ModController {
 
     @RequestMapping(value = {"/home"}, method = RequestMethod.GET)
     public String home() {
-        return "mod_startup_page_web";
+        return "mod_startup_page";
     }
 
     @RequestMapping(value = {"/createplan"}, method = RequestMethod.GET)
