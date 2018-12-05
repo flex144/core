e41f7bd00d47289673e4237010137d10f4c8f1d5
Felix Wilhelm <wilhel40@gw.uni-passau.de>
2018-12-05 20:47:55 +0100

    T2-65 #in-progress Added tests for Controller, and access based on role.

---
+++ src/test/java/de/ep/team2/core/ControllerTest.java
@@ -0,0 +1,72 @@
+package de.ep.team2.core;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.junit.Assert.assertTrue;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ControllerTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setup (){
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    public void getLogin() throws Exception{
+        ResultActions action = mvc.perform(get("/"));
+        int status = action.andReturn().getResponse().getStatus();
+        assertTrue("expected status code = 200; current status code = " + status, status == 200);
+    }
+
+    @WithMockUser
+    @Test
+    public void getUser() throws Exception {
+        ResultActions action = mvc.perform(get("/users/1"));
+        assertTrue(action.andReturn().getResponse().getContentAsString().contains("Heinrich"));
+    }
+
+    @Test
+    public void getUserPageWithoutLogin () throws Exception {
+        ResultActions action = mvc.perform(get("/user/home"));
+        int status = action.andReturn().getResponse().getStatus();
+        assertTrue("expected status code = 302; current status code = " + status, status == 302);
+        String redirectURL = action.andReturn().getResponse().getHeader("Location");
+        assertTrue("redirect to login page", redirectURL.equals("http://localhost/login"));
+    }
+
+    @WithMockUser
+    @Test
+    public void getUserAccess () throws Exception {
+        ResultActions action = mvc.perform(get("/user/home"));
+        int status = action.andReturn().getResponse().getStatus();
+        assertTrue("expected status = 200, current status = " + status, status == 200);
+        action = mvc.perform(get("/mods/home"));
+        status = action.andReturn().getResponse().getStatus();
+        assertTrue("expected status = 403, current status = " + status, status==403);
+    }
+
+}
--- src/test/java/de/ep/team2/core/CoreApplicationTests.java
+++ src/test/java/de/ep/team2/core/CoreApplicationTests.java
@@ -1,25 +1,58 @@
 package de.ep.team2.core;
 
-import de.ep.team2.core.service.DataBaseService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.*;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest
 public class CoreApplicationTests {
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    private WebApplicationContext context;
 
+    private MockMvc mvc;
+
+    @Before
+    public void setup (){
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @WithMockUser
     @Test
-    public void getUser() {
+    public void getUser() throws Exception {
+        //Get User-profile from existing user with user id
+        ResultActions action = mvc.perform(get("/users/1"));
+        assertTrue(action.andReturn().getResponse().getContentAsString().contains("Heinrich"));
+
+        //Get User-profile from non-existing user
+        action = mvc.perform(get("/users/235"));
+        assertTrue(action.andReturn().getResponse().getContentAsString()
+                .contains("Benutzer nicht gefunden!"));
+
+        //Get User-profile from existing user with e-mail
+        action = mvc.perform(get("/users/alex@gmail.com"));
+        assertTrue(action.andReturn().getResponse().getContentAsString().contains("Reißig"));
+
+        /**
         assertTrue(restTemplate.getForObject("/users/1", String.class)
                 .contains("Heinrich"));
         assertTrue(restTemplate.getForObject("/users/235", String.class)
@@ -27,10 +60,20 @@ public class CoreApplicationTests {
         assertTrue(restTemplate.getForObject("/users/alex@gmail.com",
                 String.class)
                 .contains("Reißig"));
+         */
     }
 
+    @WithMockUser
     @Test
-    public void deleteUser() {
+    public void deleteUser() throws Exception {
+        //Delete User with ID 1
+        TestRestTemplate restTemplate = new TestRestTemplate();
+        restTemplate.delete("http://localhost:8080/users/1");
+        ResultActions action = mvc.perform(get("/users/1"));
+        String result = action.andReturn().getResponse().getContentAsString();
+        assertTrue(action.andReturn().getResponse().getContentAsString().contains("Benutzer nicht gefunden!"));
+
+        /**
         restTemplate.delete("http://localhost:8080/users/1");
         assertTrue(restTemplate.getForObject("/users/1", String.class)
                 .contains("Benutzer nicht gefunden!"));
@@ -42,10 +85,12 @@ public class CoreApplicationTests {
                 "http://localhost:8080/users/eg93434", HttpMethod.DELETE, null,
                 String.class);
         assertTrue(response2.getBody().contains("ID nicht valide!"));
+         */
     }
 
     @Test
     public void addUser() {
+        /*
         assertTrue(restTemplate.getForObject("/users/Test@gmail.com",
                 String.class)
                 .contains("Benutzer nicht gefunden!"));
@@ -54,5 +99,6 @@ public class CoreApplicationTests {
         assertTrue(restTemplate.getForObject("/users/Test@gmail.com",
                 String.class)
                 .contains("Test@gmail.com"));
+                */
     }
 }
