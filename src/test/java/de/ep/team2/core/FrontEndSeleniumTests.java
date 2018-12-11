94662206908ae6c3748a6b73e4c33a79fd5c6833
reissi01 <REISSI01@gw.uni-passau.de>
2018-12-11 15:09:32 +0100

    Added class for front-end tests with selenium

---
+++ src/test/java/de/ep/team2/core/FrontEndSeleniumTests.java
@@ -0,0 +1,27 @@
+package de.ep.team2.core;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FrontEndSeleniumTests {
+
+    @Test
+    public void exampleTestYoutube() {
+        //System.setProperty("webdriver.gecko.driver", //URL);
+
+        WebDriver driver = new FirefoxDriver();
+
+        driver.navigate().to("https://www.youtube.com/");
+
+        String currentTitle = driver.getTitle();
+
+        if(currentTitle.contentEquals("YouTube")) {
+            System.out.println("Test for Youtube passed!");
+        } else {
+            System.out.println("Test for Youtube failed");
+        }
+
+        driver.quit();
+    }
+}
